abdb8665d9ca25957836a65633e1fc5d691dbe2a
amitlimbasiya <amit@webenix.net>
2022-08-30 16:56:56 +0530

    remove package

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,3 @@
-import Router from 'next/router';
-import NProgress from 'nprogress';
-import 'nprogress/nprogress.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SSRProvider from 'react-bootstrap/SSRProvider';
 import Header from '../components/Header';
@@ -8,11 +5,6 @@ import Footer from '../components/Footer';
 import FooterScript from '../components/FooterScript';
 
 import '../styles/globals.css';
-NProgress.configure(
-  {showSpinner:false}
-)
-Router.events.on('routeChangeStart', () => NProgress.start()); Router.events.on('routeChangeComplete', () => NProgress.done()); Router.events.on('routeChangeError', () => NProgress.done());
-
 
 function MyApp({ Component, pageProps }) { 
   return <SSRProvider>
