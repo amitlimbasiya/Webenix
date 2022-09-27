edf34bb6ad8fc30abddef8e01ead42c80e25e5c9
rahulsagar-webenix <amit@webenix.net>
2022-09-27 17:57:07 +0530

    try to setup .env variable

--- pages/_document.js
+++ pages/_document.js
@@ -32,17 +32,22 @@ class MyDocument extends Document {
             `}
           </Script> */}
 
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+          <Script
+            strategy="lazyOnload"
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+          />
+          <Script id="ga-script" strategy="lazyOnload">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
+                page_path: window.location.pathname,
+              });
+                  `}
+          </Script>
 
-          ga('create', 'UA-150436646-1', 'auto');
-          ga('send', 'pageview');
-        `}
-      </Script>
+    
 
           <Script type="application/ld+json">
               {`
