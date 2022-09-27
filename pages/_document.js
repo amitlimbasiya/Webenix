287c455ea697a43a5edb9a1dd4e74add328fc3d4
rahulsagar-webenix <amit@webenix.net>
2022-09-27 17:34:08 +0530

    Using analytics.js

--- pages/_document.js
+++ pages/_document.js
@@ -21,7 +21,7 @@ class MyDocument extends Document {
             <script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/6378971.js"></script>
           {/* End of HubSpot Embed Code */}
           {/* Global site tag (gtag.js) - Google Analytics */}
-          <Script src="https://www.googletagmanager.com/gtag/js?id=UA-150436646-1" strategy="afterInteractive"/>
+          {/* <Script src="https://www.googletagmanager.com/gtag/js?id=UA-150436646-1" strategy="afterInteractive"/>
           <Script id="google-analytics" strategy="afterInteractive">
             {`
               window.dataLayer = window.dataLayer || [];
@@ -30,8 +30,20 @@ class MyDocument extends Document {
 
               gtag('config', 'UA-150436646-1');
             `}
-          </Script>
-          
+          </Script> */}
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-150436646-1', 'auto');
+          ga('send', 'pageview');
+        `}
+      </Script>
+
           <Script type="application/ld+json">
               {`
                 "@context": "https://schema.org/",
