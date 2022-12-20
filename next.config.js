362a924e9e6ddcf57b31646b64768c2d417d8896
amit <amit@webenix.net>
2022-12-20 17:17:30 +0530

    Redirection add

--- next.config.js
+++ next.config.js
@@ -20,6 +20,51 @@ module.exports = {
         destination: '/work',
         permanent: true,
       },
+      {
+        source: '/portfolio/usa-pipelining',
+        destination: '/work/usa-pipelining',
+        permanent: true,
+      },
+      {
+        source: '/portfolio/southerntradelines',
+        destination: '/work/southerntradelines',
+        permanent: true,
+      },
+      {
+        source: '/portfolio/diningedge',
+        destination: '/work/diningedge',
+        permanent: true,
+      },
+      {
+        source: '/get-in-touch',
+        destination: '/contact',
+        permanent: true,
+      },
+      {
+        source: '/logo-brand-identity-guidelines',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/about-us',
+        destination: '/about',
+        permanent: true,
+      },
+      {
+        source: '/blog/why-outsourcing-top-5-reasons-benefits-instead-hiring',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/blog/why-outsourcing-top-5-reasons-benefits-instead-hiring#!',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/blog/#!',
+        destination: '/blog',
+        permanent: true,
+      }
     ]
   },
 };
