0710072655d0ce68a35ae3ff5e75836e06e7cfac
amitlimbasiya <amit@webenix.net>
2023-03-08 00:18:35 +0530

    update blog date

--- pages/blog/why-hiring-an-offshore-next-js-developer-may-be-the-right-choice-for-your-project.js
+++ pages/blog/why-hiring-an-offshore-next-js-developer-may-be-the-right-choice-for-your-project.js
@@ -29,7 +29,7 @@ const SingleBlog = () => {
               <h1 className="blog-title">
                 Why Hiring an Offshore Next JS Developer May Be the Right Choice for Your Project
               </h1>
-              <span className="blog-date">February 28, 2023</span>
+              <span className="blog-date">February 27, 2023</span>
             </Col>
             <Col lg={12} md={12} className="singleblog-banner-img">
               <Image
